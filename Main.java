b8732b9cf3a2695c4331d4e551db61f1e8163bcc
diegotco <diegotco@yahoo.com>
2021-02-10 12:38:02 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+class Main {
+    public static void main(String[] args) throws Exception {
+       int numeroChico = 0;
+       int numeroGrande = 10;
+        
+        while (numeroChico < numeroGrande){
+            
+            System.out.println(numeroChico);
+            numeroChico++;
+            
+        }
+        
+        
+    }
+}
